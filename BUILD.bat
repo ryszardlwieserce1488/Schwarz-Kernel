134655d0a2c126131f3aafe6b5d364061c6ad326
zegar <ghjeuam@gmail.com>
2026-04-19 09:22:34 +0200

    Spolszczenie logów

--- BUILD.bat
+++ BUILD.bat
@@ -1,47 +1,38 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
-
 set "SRC_DIR=%~dp0"
 if "%SRC_DIR:~-1%"=="\" set "SRC_DIR=%SRC_DIR:~0,-1%"
-
 set "CFG=%~1"
 if "%CFG%"=="" set "CFG=Release"
-
 set "OUT_DIR=%SRC_DIR%\x64\%CFG%"
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
-
-:: 1. Definiujemy wsp?lne flagi w jednej zmiennej
 set "CFLAGS=-target x86_64-unknown-elf -ffreestanding -fno-stack-protector -fno-exceptions -fno-rtti -fno-pic -fno-pie -mno-red-zone -ffunction-sections -fdata-sections -nostdlib -I"%SRC_DIR%""
 
-echo [0/4] Deleting old ".o" files...
+echo [0/4] Usuwams stare pliki ".o"...
 del /q "%OUT_DIR%\*.o" 2>nul
 
-echo [1/4] Compiling C++ files...
-:: 2. P?tla przechodz?ca po li?cie plik?w
-:: P?tla przechodzi przez ka?dy plik .cpp w SRC_DIR
+echo [1/4] Kompiluję pliki źródłowe C/C++...
 for %%F in ("%SRC_DIR%\*.cpp") do (
-    echo   Compiling %%~nxF...
+    echo   Kompiluję %%~nxF...
     clang %CFLAGS% -c "%%F" -o "%OUT_DIR%\%%~nF.o" || exit /b 1
 )
 
-echo [2/4] Assembling NASM files...
-:: Pętla przechodzi przez każdy plik .asm w SRC_DIR
+echo [2/4] Kompiluję pliki źródłowe ASM...
 for %%F in ("%SRC_DIR%\*.asm") do (
-    echo    Assembling %%~nxF...
+    echo    Kompiluję %%~nxF...
     nasm -f elf64 "%%F" -o "%OUT_DIR%\%%~nF.o" || exit /b 1
 )
 
-echo [3/4] Linking image...
-:: Tutaj u?ywamy maski *.o, aby automatycznie zlinkowa? wszystko, co si? skompilowa?o
+echo [3/4] Konsoliduję projekt...
 ld.lld -m elf_x86_64 ^
        -T "%SRC_DIR%\linker.ld" ^
        -Map "%OUT_DIR%\kernel.map" ^
        -o "%OUT_DIR%\kernel.elf" ^
        "%OUT_DIR%\*.o" || exit /b 1
 
-echo [4/4] Converting ELF to flat binary...
+echo [4/4] Porządkuję plik binarny...
 llvm-objcopy -O binary "%OUT_DIR%\kernel.elf" "%OUT_DIR%\kernel.bin" || exit /b 1
 
 echo.
-echo Build OK: "%OUT_DIR%\kernel.bin"
+echo Kompilacja pomyślna: "%OUT_DIR%\kernel.bin"
 exit /b 0
